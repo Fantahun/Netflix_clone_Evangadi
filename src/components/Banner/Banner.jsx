e1f46542b4d4c8d2c3d65133f468136c936b3f3d
Fantahun <Fantahun731@gmail.com>
2024-09-07 12:36:39 +0300

    final touches - added play trailer functionality to the banner, changed the banner to fetch top trending movies and done some styling modifications

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,26 +3,72 @@ import { My_axios_Instance, images_baseURL } from "../../utils/axios";
 import requests from "../../utils/requests";
 import "./banner.css";
 
+import movieTrailer from "movie-trailer";
+import Youtube from "react-youtube";
+
 function Banner() {
   const [movie, setMovie] = useState({});
+  const [trailerUrl, setTrailerUrl] = useState("");
+  const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
     async function fetchMovies() {
       try {
-        const response = await My_axios_Instance.get(
-          requests.fetchNetflixOriginals
-        );
-        // console.log(JSON.stringify(response));
+        const response = await My_axios_Instance.get(requests.fetchTrending);
         const results = await response.data.results;
-        // console.log("Final result:", results);
         const randomPick = Math.random() * results.length;
         setMovie(results[Math.floor(randomPick)]);
       } catch (error) {
-        console.log("Error occured while fetching data " + error);
+        console.log("Error occurred while fetching data " + error);
       }
     }
     fetchMovies();
   }, []);
+
+  // Handle button states / clicks
+  const handleButtonClick = () => {
+    if (isPlaying) {
+      // Close the video
+      setTrailerUrl(""); // Stop the video by clearing the trailer URL
+    } else {
+      // Play the video
+      playTrailer(movie);
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  // Play video functionality
+  const playTrailer = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl(""); // Close the video if it's already playing
+    } else {
+      movieTrailer(
+        movie?.title ||
+          movie?.name ||
+          movie?.original_name ||
+          movie?.original_title ||
+          ""
+      )
+        .then((url) => {
+          if (!url) {
+            return false;
+          }
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => {
+          console.error("Error fetching the trailer:", error);
+        });
+    }
+  };
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
   return (
     <>
@@ -32,24 +78,35 @@ function Banner() {
           backgroundImage: `url(${images_baseURL}${movie?.backdrop_path})`,
         }}
       >
-        {/* {console.log(
-          ` test image url: ${images_baseURL}${movie?.backdrop_path}`
-        )} */}
-
         <div className="featured">
-          <div className="featured-title">
-            <h1>{movie?.original_name}</h1>
+          <div className="featured_left">
+            <div className="featured-title">
+              <h1>
+                {movie?.title ||
+                  movie?.name ||
+                  movie?.original_name ||
+                  movie?.original_title ||
+                  ""}
+              </h1>
+            </div>
+            <div className="featured-buttons">
+              <button onClick={handleButtonClick} className="play_btn">
+                {isPlaying ? "Close Trailer" : "Play Trailer"}
+              </button>
+              <button className="my_list_btn">My List</button>
+            </div>
+            <div className="featured-description">
+              <p>{String(movie?.overview).substring(0, 150) + "..."}</p>
+            </div>
           </div>
-          <div className="featured-buttons">
-            <button className="play_btn">Play</button>
-            <button className="my_list_btn">My List</button>
-          </div>
-          <div className="featured-description">
-            <p>{String(movie?.overview).substring(0,150)+'...'}</p>
+          <div
+            className="trailer_container"
+            style={{ padding: "15px", width: "100%" }}
+          >
+            {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
           </div>
         </div>
-        {/* <div className="featured_fade_bottom"></div> */}
-      <div className="featured_fade_bottom"></div>
+        <div className="featured_fade_bottom"></div>
       </div>
     </>
   );
--- src/components/Rows/Row/Row.jsx
+++ src/components/Rows/Row/Row.jsx
@@ -1,21 +1,20 @@
 import React, { useEffect, useState } from "react";
 import "./row.css";
 import { My_axios_Instance, images_baseURL } from "../../../utils/axios";
-import movieTrailer from 'movie-trailer';
+import movieTrailer from "movie-trailer";
 import Youtube from "react-youtube";
 
 function Row({ title, fetchURL, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
 
-
   useEffect(() => {
     async function fetchMovies() {
       try {
         const response = await My_axios_Instance.get(fetchURL);
-        console.log(response);
+        // console.log(response);
         const results = await response.data.results;
-        console.log(results);
+        // console.log(results);
         setMovies(results);
       } catch (error) {
         console.log("error occured while fetching data: " + error);
@@ -27,32 +26,38 @@ function Row({ title, fetchURL, isLargeRow }) {
 
   const playTrailer = (movie) => {
     if (trailerUrl) {
-      setTrailerUrl('');
+      setTrailerUrl("");
     } else {
-      movieTrailer(movie?.title || movie?.name || movie?.original_name || '')
+      movieTrailer(
+        movie?.title ||
+          movie?.name ||
+          movie?.original_name ||
+          movie.original_title ||
+          ""
+      )
         .then((url) => {
-          if(!url){
+          if (!url) {
             return false;
           }
-          console.log(url);
+          // console.log(url);
           const urlParams = new URLSearchParams(new URL(url).search);
-          console.log(urlParams);
-          console.log(urlParams.get('v'));
-          setTrailerUrl(urlParams.get('v'));
-        }).catch((error) => {
-          console.error('Error fetching the trailer:', error);
+          // console.log(urlParams);
+          // console.log(urlParams.get("v"));
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => {
+          console.error("Error fetching the trailer:", error);
         });
     }
   };
-  
+
   const opts = {
-    height: '390',
-    width: '100%',
+    height: "390",
+    width: "100%",
     playerVars: {
       autoplay: 1,
     },
-  }
-
+  };
 
   return (
     <div className="row_container">
@@ -61,12 +66,14 @@ function Row({ title, fetchURL, isLargeRow }) {
       </div>
       <div className="movie_list_holder">
         {movies?.map((movie, index) => {
+          // console.log(movie)
           return (
             <>
-              {console.log(index)}
-              {console.log(movie)}
+              {/* {console.log(index)}
+              {console.log(movie)} */}
               <img
-              onClick= {()=>playTrailer(movie)}
+                key={index}
+                onClick={() => playTrailer(movie)}
                 className={`row_poster ${isLargeRow && "row_posterLarge"}`}
                 src={`${images_baseURL}${
                   isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -77,8 +84,8 @@ function Row({ title, fetchURL, isLargeRow }) {
           );
         })}
       </div>
-      <div style={{padding:"15px"}}>
-        {trailerUrl &&  <Youtube videoId={trailerUrl} opts={opts}/>}
+      <div style={{ padding: "15px" }}>
+        {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
       </div>
     </div>
   );
